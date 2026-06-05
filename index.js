1298946eea09148a8a5c3e3df8e5f330b987383f
phantekzy <mainilotfi@gmail.com>
2026-06-05 22:51:24 +0100

    setting the values of the fov , aspect , near and far

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import * as THREE from "three";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(w, h);
 
+renderer.setSize(w, h);
 document.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
