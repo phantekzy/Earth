0cad3a23be3690fb2ff051f8e4f16861318cc76a
phantekzy <mainilotfi@gmail.com>
2026-06-05 22:41:01 +0100

    added the four main settings for a camera in Threejs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-const camera = new THREE.PerspectiveCamera();
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 
