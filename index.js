a4f0343d446ffd95d64b07be4e8c1084a6c990eb
phantekzy <mainilotfi@gmail.com>
2026-06-05 22:39:33 +0100

    appended the domElement and added a camera

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ import * as THREE from "three";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
+const camera = new THREE.PerspectiveCamera();
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
+
+document.appendChild(renderer.domElement);
